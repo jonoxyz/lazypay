06f0ea837486249102e61c3c4984a774145157f8
jedxyz <jedwards020@gmail.com>
2024-02-23 14:45:45 +1100

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,20 @@
 # built by Jonathan Edwards because typing in
 # your sign on and off times is just too hard.
 
-# Visit https://www.lazypay.xyz to download the latest version
-# If you find a bug use the contact form on the website
+# Visit https://www.lazypay.xyz to download the latest version.
+# If you find a bug use the contact form on the website.
 
-#Enter your login details for Metro Go apps
-#These are different from your computer login datails
+# Enter your login details for Metro Go apps.
+# These are different from your computer login datails.
+# Do not remove quotation marks.
 
 username = "firstname.lastname"
 password = "password"
 
-#If you are experiencing crashing when loading your shifts
-#it may be due to a slow connection.
-#Increase the below number to pause for longer on each page
-#allowing your browser to load each shift.
-#Faster connections may reduce this to speed up gathering shift data.
+# If you are experiencing crashing when loading your shifts
+# it may be due to a slow connection.
+# Increase the below number to pause for longer on each page
+# allowing your browser to load each shift.
+# Faster connections may reduce this to speed up gathering shift data.
 
 loading_speed = 1
